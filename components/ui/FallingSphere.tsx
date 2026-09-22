52c5e7b9a65f6c73569c8f0447297b8398899c24
vigneshrao77 <peggarlavigneshrao1@gmail.com>
2026-09-23 01:41:11 +0530

    feat: upgrade falling sphere with physics-based bouncing and motion trail

--- components/ui/FallingSphere.tsx
+++ components/ui/FallingSphere.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import './FallingSphere.css';
+import React from "react";
+import "./FallingSphere.css";
 
 export const FallingSphere = () => {
   return (
     <div className="falling-sphere-container" aria-hidden="true">
       <div className="fs-x">
         <div className="fs-y">
+          <div className="fs-trail" />
           <div className="fs-ball">
+            <div className="fs-core" />
             <div className="fs-glow" />
-            <div className="fs-trail" />
           </div>
         </div>
       </div>
